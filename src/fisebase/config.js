8a32c8d37d3d3592a914e1e69c2ad7d7a1a0566d
user <user@usernoMacBook-Pro.local>
2025-05-01 14:34:05 +0700

    nguyenvangiau

--- src/fisebase/config.js
+++ src/fisebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYaqJfOJW4LhJg5NZ3kZe0cxdregJeV7M",
-  authDomain: "quiz-6b841.firebaseapp.com",
-  projectId: "quiz-6b841",
-  storageBucket: "quiz-6b841.firebasestorage.app",
-  messagingSenderId: "517720295241",
-  appId: "1:517720295241:web:4cab4552431d553bccfc98",
-  measurementId: "G-JF57TBCEYY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
